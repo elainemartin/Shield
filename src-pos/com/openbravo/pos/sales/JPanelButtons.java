893710200511784880104965167efe4c82203bd1
kh <khundman@gmail.com>
2015-02-04 12:03:16 -0800

    Change text area to editor pane

--- src-pos/com/openbravo/pos/sales/JPanelButtons.java
+++ src-pos/com/openbravo/pos/sales/JPanelButtons.java
@@ -229,23 +229,20 @@ public class JPanelButtons extends javax.swing.JPanel {
 
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jEditorPane1 = new javax.swing.JEditorPane();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jEditorPane2 = new javax.swing.JEditorPane();
 
         jScrollPane2.setViewportView(jTextPane1);
+
+        jScrollPane3.setViewportView(jEditorPane1);
 
         setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         setPreferredSize(new java.awt.Dimension(450, 77));
         setRequestFocusEnabled(false);
-
-        jTextArea1.setColumns(20);
-        jTextArea1.setLineWrap(true);
-        jTextArea1.setRows(50);
-        jTextArea1.setText("Click here to scan customer ID...");
-        jTextArea1.setWrapStyleWord(true);
-        jScrollPane1.setViewportView(jTextArea1);
 
         jButton1.setText("Check Age");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -260,6 +257,10 @@ public class JPanelButtons extends javax.swing.JPanel {
                 jButton2ActionPerformed(evt);
             }
         });
+
+        jEditorPane2.setText("Click here to scan ID...");
+        jEditorPane2.setToolTipText("");
+        jScrollPane1.setViewportView(jEditorPane2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -281,19 +282,20 @@ public class JPanelButtons extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(7, 7, 7)
+                        .addComponent(jButton2)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         //JOptionPane.showMessageDialog(null,"the button worked","AGE VERIFICATION",JOptionPane.INFORMATION_MESSAGE);
-        String age = jTextArea1.getText().toLowerCase();
+        String age = jEditorPane2.getText().toLowerCase();
         String dob1 = "Date of birth".toLowerCase();
         String dob2 = "DOB".toLowerCase();
         String dob3 = "Birthday".toLowerCase();
@@ -365,16 +367,18 @@ public class JPanelButtons extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        jTextArea1.setText("");
+        jEditorPane2.setText("");
     }//GEN-LAST:event_jButton2ActionPerformed
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JEditorPane jEditorPane1;
+    private javax.swing.JEditorPane jEditorPane2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
     
